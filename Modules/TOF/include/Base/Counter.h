fffc7b70822214ebb99a0014326294b403707972
Francesca Ercolessi <francesca.ercolessi@cern.ch>
2021-02-03 10:03:58 +0100

    Removed redundant definition of function Count()

--- Modules/TOF/include/Base/Counter.h
+++ Modules/TOF/include/Base/Counter.h
@@ -44,10 +44,10 @@ class Counter
   ~Counter() = default;
 
   /// Functions to increment a counter
-  /// @param v Index in the counter array to increment
-  void Count(const unsigned int& v);
   /// @param v Index in the counter array to increment, @param weight weight to add to the array element
   void Add(const unsigned int& v, const unsigned int& weight);
+
+  void Count(const unsigned int& v);
 
   /// Function to reset counters to zero
   void Reset();
@@ -97,18 +97,6 @@ class Counter
 // #define ENABLE_PRINT_HISTOGRAMS_MODE // Flag used to enable more printing and more debug
 
 template <const unsigned int size, const char* labels[size]>
-void Counter<size, labels>::Count(const unsigned int& v)
-{
-  if (v > size) {
-    LOG(FATAL) << "Incrementing counter too far! " << v << "/" << size;
-  }
-#ifdef ENABLE_COUNTER_DEBUG_MODE
-  LOG(INFO) << "Incrementing " << v << "/" << size << " to " << counter[v];
-#endif
-  counter[v]++;
-}
-
-template <const unsigned int size, const char* labels[size]>
 void Counter<size, labels>::Add(const unsigned int& v, const unsigned int& weight)
 {
   if (v > size) {
@@ -118,6 +106,12 @@ void Counter<size, labels>::Add(const unsigned int& v, const unsigned int& weigh
   LOG(INFO) << "Incrementing " << v << "/" << size << " of " << weight << " to " << counter[v];
 #endif
   counter[v] += weight;
+}
+
+template <const unsigned int size, const char* labels[size]>
+void Counter<size, labels>::Count(const unsigned int& v)
+{
+  Add(v, 1);
 }
 
 template <const unsigned int size, const char* labels[size]>
